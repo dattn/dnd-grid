840fd5148adfafb1d32d4c283ac253d417ea989f
daniel@duton.lu <daniel@duton.lu>
2023-02-27 14:48:31 +0100

    create hash based on package.json checksum

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 import crypto from 'node:crypto'
 import { resolve } from 'node:path'
+import { readFileSync } from 'node:fs'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const cssModulesHash = crypto.randomBytes(20).toString('base64').replaceAll(/[^0-9a-z]/ig, '').substring(0, 6)
+
+const cssModulesHash = crypto
+    .createHash('sha256')
+    .update(readFileSync(resolve('./package.json')))
+    .digest('base64')
+    .replaceAll(/[^0-9a-z]/ig, '')
+    .substring(0, 6)
 
 // https://vitejs.dev/config/
 export default defineConfig({
