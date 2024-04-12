ee3cdcbd9f0f4b465d0ade763157e88791265068
daniel@duton.lu <daniel@duton.lu>
2024-04-12 15:20:27 +0200

    remove unused code

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-import crypto from 'node:crypto'
 import { resolve } from 'node:path'
-import { readFileSync } from 'node:fs'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-
-const cssModulesHash = crypto
-    .createHash('sha256')
-    .update(readFileSync(resolve('./package.json')))
-    .digest('base64')
-    .replaceAll(/[^0-9a-z]/ig, '')
-    .substring(0, 6)
 
 // https://vitejs.dev/config/
 export default defineConfig({
